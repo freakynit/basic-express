a6e249e33738e2cf7e243e2600c3c8f05dfc075d
AshesVas <ashes.vats@gmail.com>
2016-04-21 16:12:49 +0530

    added mongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express')
   , app = express()
   , bodyParser = require('body-parser')
   , port = process.env.PORT || 3000
+  , db = require('./model/db');
+
 
 app.set('views', __dirname + '/views')
 app.engine('jade', require('jade').__express)
@@ -14,6 +16,18 @@ app.use(bodyParser.urlencoded({extended: true}))
 //middelware to load controllers
 app.use(require('./controllers'))
 
-app.listen(port, function() {
-  console.log('Listening on port ' + port)
+db.connect('mongodb://developer:bridgeit@ds013908.mongolab.com:13908/sp',function(){
+    //callback when connect success
+});
+
+db.get().connection.on('connected', function () {
+  console.log('Mongoose connected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  db.get().connection.close(function () {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
 });
--- controllers/index.js
+++ controllers/index.js
@@ -9,4 +9,5 @@ router.get('/',function(req,res){
     res.send('This is main controller');
 });
 
+
 module.exports=router;
